bc090bdec364bf6523cd41a54e2ec66d7ebf480d
Matheus Pinho <matheus_dmp19@hotmail.com>
2023-02-14 12:48:04 -0300

    Fix nodegroup

--- modules/nodes/node-group.tf
+++ modules/nodes/node-group.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_node_group" "node_group" {
   
-  cluster_name = var.cluster_name
+  cluster_name = var.eks_cluster.name
   node_group_name = "${var.cluster_name}-node-group"
   node_role_arn = aws_iam_role.eks_nodes_role.arn
 
